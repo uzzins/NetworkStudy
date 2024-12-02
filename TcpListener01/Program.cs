0f501961edaac0a50a7533803570255a4f84d680
uzinsea@gmail.com <uzinsea@gmail.com>
2024-12-02 22:50:47 +0900

    2024.12.02 네트워크 클래스 공부

---
+++ TcpListener01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TcpListener01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            TcpListener tcpListener = new TcpListener(ip, 13); //지정된 ip와 port 번호에서 연결을 수신 대기하겠다는 의미.
+            Console.WriteLine("{0}", tcpListener.LocalEndpoint.ToString());
+            Console.ReadKey();
+        }
+    }
+}
