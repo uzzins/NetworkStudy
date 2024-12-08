4d764c3f7d3bc3a7e77aa2f44443e304edca7b42
uzinsea@gmail.com <uzinsea@gmail.com>
2024-12-09 07:08:44 +0900

    2024.12.09 네트워크 클래스 공부(3)

---
+++ BInaryRead0120241209/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BInaryRead01_20241209
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool YesNo;
+            int Number;
+            float Pi;
+            string message;
+            TcpClient tcpClient = new TcpClient("127.0.0.1", 3000);
+            NetworkStream ns = tcpClient.GetStream();
+
+            using (BinaryReader reader = new BinaryReader(ns)) { 
+               YesNo = reader.ReadBoolean();
+                Number = reader.ReadInt32();
+                Pi = reader.ReadSingle();
+                message = reader.ReadString();
+            }
+            ns.Close();
+            tcpClient.Close();
+
+            Console.WriteLine(YesNo);
+            Console.WriteLine(Number);
+            Console.WriteLine(Pi);
+            Console.WriteLine(message);
+
+        }
+    }
+}
---
+++ BinaryWriter01_20241209/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryWriter01_20241209
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpListener tcpListener = new TcpListener(IPAddress.Any, 3000);
+            tcpListener.Start();
+
+            TcpClient tcpClient = tcpListener.AcceptTcpClient();
+            NetworkStream ns = tcpClient.GetStream();
+
+            using(BinaryWriter bw = new BinaryWriter(ns))
+            {
+                bool YesNo = true;
+                int Number = 12;
+                float Pi = 3.14f;
+                string message = "Hello World!";
+
+                bw.Write(YesNo);
+                bw.Write(Number);
+                bw.Write(Pi);
+                bw.Write(message);
+            }
+            ns.Close();
+            tcpClient.Close();
+            tcpListener.Stop();
+
+        }
+    }
+}
